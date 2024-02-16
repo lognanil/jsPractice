5f5879f1f591c89691386aa9e3a64c3b44388c3c
lognanil <69664288+lognanil@users.noreply.github.com>
2024-02-16 08:59:41 +0000

    basic practice 2

--- basics/test.js
+++ basics/test.js
@@ -1,3 +1,9 @@
 //to run in terminal -  node basics/test.js
-// 
-console.log("hello world");
+// click on fourth icon in left menu, click on + symbols, 
+// put a message , then onclick on commit button
+// click on three dot , then push, now you can see your file in github repo
+// after doing excercise, must close the codespace in github
+
+
+console.log("hello");
+
